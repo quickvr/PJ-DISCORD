1802a65d5c2d302194a092b0a62e647fd23bfc4e
quickvr <155215898+quickvr@users.noreply.github.com>
2024-09-20 13:59:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,62 +1,21 @@
-// server.js
-// where your node app starts
+// script.js
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
-const server = require("http").createServer(app);
-const io = require("socket.io")(server);
-let messages = []
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('webgl');
+document.getElementById('gameContainer').appendChild(canvas);
 
-let numUsers = 0;
-let usernameList = [];
+function init() {
+    // Initialize WebGL and set up the game environment
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    // Additional setup code here
+}
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+function gameLoop() {
+    // Main game loop
+    requestAnimationFrame(gameLoop);
+    // Game rendering and logic here
+}
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
-});
-
-io.on("connection", socket => {
-  console.log("a user connected!");
-  ++numUsers;
-  console.log("There are currently " + numUsers + " users.");
-  io.emit("update user count", numUsers)
-  for (let i = 0; i < messages.length; i++) {
-    socket.emit("display message", messages[i])
-  }
-
-  socket.on("disconnecting", () => {
-    usernameList = []
-    io.emit("return username");
-    console.log("a user disconnected.");
-    --numUsers;
-    console.log("There are currently " + numUsers + " users.");
-    io.emit("update user count", numUsers)
-    if (numUsers == 0 && Array.isEmpty(usernameList)) {
-      messages = []
-    }
-  });
-
-  socket.on("entered username", username => {
-    usernameList.push(username);
-    io.emit("update user list", usernameList);
-  });
-  
-  socket.on("send message", (data) => {
-    //access message with data[0], name with data[1]
-    let message = data[0]
-    let sender = data[1]
-    messages.push([message, sender])
-    io.emit("display message", [message, sender])
-  })
-});
-
-// listen for requests :)
-const listener = server.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+window.onload = init;
+gameLoop();
